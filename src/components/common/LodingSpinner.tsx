5f0a558f7fe22e4d26f6e07aa2892d8080139e07
Suki <tues1205@naver.com>
2025-06-02 17:04:03 +0900

    ✨ : 로딩스피너 컴포넌트

---
+++ src/components/common/LodingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HashLoader } from "react-spinners";
+
+const LodingSpinner = () => {
+  return (
+    <div>
+      <HashLoader color="#317645" size={30} />
+    </div>
+  );
+};
+
+export default LodingSpinner;
