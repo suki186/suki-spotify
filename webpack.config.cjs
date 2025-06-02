572aa93be0f676b3dbf67fd953d677ac788c14d2
Suki <tues1205@naver.com>
2025-06-02 15:54:45 +0900

    🎉 : 프로젝트 초기 세팅

---
+++ webpack.config.cjs
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/main.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js",
+      publicPath: "/",
+      clean: true,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+      }),
+      new Dotenv(),
+    ],
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
+};
