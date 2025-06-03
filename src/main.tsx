2af5eb1d03420b4a1df6a75e9a2daf97adafdb4e
Suki <tues1205@naver.com>
2025-06-03 16:35:03 +0900

    💄 : Theme 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import theme from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 createRoot(document.getElementById("content")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
