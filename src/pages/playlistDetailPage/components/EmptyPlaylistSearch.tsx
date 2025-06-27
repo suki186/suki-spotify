2cda8d1621a2198aedd332b4f173aec903d78ada
Suki <tues1205@naver.com>
2025-06-27 14:51:42 +0900

    ✨ : 검색결과 무한스크롤

--- src/pages/playlistDetailPage/components/EmptyPlaylistSearch.tsx
+++ src/pages/playlistDetailPage/components/EmptyPlaylistSearch.tsx
@@ -1,4 +1,10 @@
-import { InputAdornment, styled, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  InputAdornment,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import React, { useState } from "react";
 import { SEARCH_TYPE } from "../../../models/search";
@@ -6,6 +12,21 @@ import useSearchItemsByKeyword from "../../../hooks/useSearchItemsByKeyword";
 import LoadingSpinner from "../../../components/common/components/LoadingSpinner";
 import ErrorMessage from "../../../components/common/components/ErrorMessage";
 import SearchResultList from "./SearchResultList";
+import { Track } from "../../../models/track";
+
+const SearchContainer = styled(Box)({
+  // 스크롤 디자인
+  padding: "16px",
+  width: "100%",
+  height: "100%",
+  overflowY: "auto",
+
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+  msOverflowStyle: "none", // IE and Edge
+  scrollbarWidth: "none", // Firefox
+});
 
 const StyledTextField = styled(TextField)(({ theme }) => ({
   width: "100%",
@@ -30,7 +51,14 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
 
 const EmptyPlaylistSearch = () => {
   const [keyword, setKeyword] = useState<string>("");
-  const { data, error, isLoading } = useSearchItemsByKeyword({
+  const {
+    data,
+    error,
+    isLoading,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useSearchItemsByKeyword({
     q: keyword,
     type: [SEARCH_TYPE.Track],
   });
@@ -41,12 +69,14 @@ const EmptyPlaylistSearch = () => {
     setKeyword(event.target.value);
   };
 
-  const tracks = data?.pages[0]?.tracks?.items ?? []; //널 병합 연산자
+  const tracks = (
+    data?.pages.flatMap((page) => page.tracks?.items) ?? []
+  ).filter((track): track is Track => !!track); //널 병합 연산자
   const hasResults = tracks.length > 0; // 검색 결과가 있는가
   const hasSearched = keyword.trim() !== ""; // 검색어가 있는가
 
   return (
-    <div>
+    <SearchContainer>
       <Typography variant="h1" my="10px">
         Let's Find a Song For Your Playlist!
       </Typography>
@@ -72,11 +102,18 @@ const EmptyPlaylistSearch = () => {
       {error && <ErrorMessage errorMessage={error.message} />}
       {isLoading && <LoadingSpinner />}
 
-      {!isLoading && hasResults && <SearchResultList list={tracks} />}
+      {!isLoading && hasResults && (
+        <SearchResultList
+          list={tracks}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+          fetchNextPage={fetchNextPage}
+        />
+      )}
       {!isLoading && hasSearched && !hasResults && (
         <Typography mt={2}>{`검색 결과가 없습니다: "${keyword}"`}</Typography>
       )}
-    </div>
+    </SearchContainer>
   );
 };
 
--- src/pages/playlistDetailPage/components/SearchResultList.tsx
+++ src/pages/playlistDetailPage/components/SearchResultList.tsx
@@ -1,18 +1,100 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import {
+  Box,
+  Button,
+  styled,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { Track } from "../../../models/track";
+import { useInView } from "react-intersection-observer";
+import LoadingSpinner from "../../../components/common/components/LoadingSpinner";
+
+const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
+  background: theme.palette.background.paper,
+  color: theme.palette.common.white,
+  maxHeight: "calc(100vh - 485px)",
+  height: "100%",
+  borderRadius: "8px",
+  overflowY: "auto",
+  "&::-webkit-scrollbar": {
+    display: "none",
+    msOverflowStyle: "none", // IE and Edge
+    scrollbarWidth: "none", // Firefox
+  },
+  marginTop: "8px",
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  width: "100%",
+  "&:hover": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  "& .MuiTableCell-root": {
+    borderBottom: "none",
+  },
+}));
+
+const AlbumImage = styled("img")({
+  borderRadius: "4px",
+  marginRight: "12px",
+});
 
 interface SearchResultListProps {
   list: Track[];
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage: () => void;
 }
 
-const SearchResultList = ({ list }: SearchResultListProps) => {
+const SearchResultList = ({
+  list,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: SearchResultListProps) => {
+  const [ref, inView] = useInView(); // 무한스크롤 옵저버
+
+  useEffect(() => {
+    // fetchNextPage 호출 추가
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
   return (
-    <div>
-      {list.map((track) => (
-        <Typography variant="h2">{track.name}</Typography>
-      ))}
-    </div>
+    <StyledTableContainer>
+      <TableBody sx={{ width: "100%" }}>
+        {list.map((track) => (
+          <StyledTableRow key={track.id}>
+            <TableCell>
+              <Box display="flex" alignItems="center">
+                <Box>
+                  <AlbumImage src={track.album?.images[0].url} width="40px" />
+                </Box>
+                <Box>
+                  <Typography fontWeight={700}>{track.name}</Typography>
+                  <Typography color="text.secondary">
+                    {track.artists ? track.artists[0].name : "Unknown Artist"}
+                  </Typography>
+                </Box>
+              </Box>
+            </TableCell>
+            <TableCell>{track.album?.name}</TableCell>
+            <TableCell>
+              <Button>Add</Button>
+            </TableCell>
+          </StyledTableRow>
+        ))}
+        {/* 무한스크롤 영역 */}
+        <div ref={ref} style={{ height: 1 }}>
+          {isFetchingNextPage && <LoadingSpinner />}
+        </div>
+      </TableBody>
+    </StyledTableContainer>
   );
 };
 
